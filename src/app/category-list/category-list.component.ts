a6b3cde13f51141c8e0833c4d23b2e6adbbed213
coavasic <coavasic96@gmail.com>
2018-02-06 08:24:12 +0100

    fix

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
-import { UserPass } from '../model/UserPass';
 import { Router } from '@angular/router';
 import { CategoryService } from '../category.service';
 
